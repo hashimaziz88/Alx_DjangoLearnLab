768f0f221150f5562b368260cbfcdb581a03204a
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-08-16 16:32:14 +0200

    commits

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,17 +1,21 @@
-from django.db import models
 from django.db.models.signals import post_save
+# relationship_app/models.py
+from django.db import models
 from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    USER_ROLES = (
+    ROLE_CHOICES = (
         ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=USER_ROLES)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
 
 
 def create_user_profile(sender, instance, created, **kwargs):
