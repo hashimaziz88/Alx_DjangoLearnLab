1814bb33c10308d859cf2ddb86a5ca429e9a2ab2
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-08-16 16:05:23 +0200

    commits taksk 2

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+# relationship_app/urls.py
+from django.urls import path
+from .views import RegisterView
+from django.contrib.auth.views import LoginView, LogoutView
+
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+# relationship_app/views.py
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.views import View
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        return render(request, 'register.html', {'form': form})
 
 
 def list_books(request):
