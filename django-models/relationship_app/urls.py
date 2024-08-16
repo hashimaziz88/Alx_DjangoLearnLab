cf90037e61fa9d754e93eab18e0a15b65285b6ce
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-08-16 16:13:54 +0200

    commits

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,6 +3,7 @@ from .views import list_books, LibraryDetailView
 # relationship_app/urls.py
 from django.urls import path
 from .views import RegisterView, LoginView, LogoutView
+from . import views
 
 
 
@@ -13,5 +14,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
 ]
