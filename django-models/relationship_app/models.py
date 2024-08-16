487c791d679091a97c1ff394517b924a8e1768a9
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-08-16 14:58:03 +0200

    update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
