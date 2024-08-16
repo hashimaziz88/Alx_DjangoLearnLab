97892f56521789a765d20645bd0501efe915ed27
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-08-16 15:26:40 +0200

    update query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,3 +3,5 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 Library = Library.objects.get(name=library_name)
 books_in_library = Library.books.all()
+Author.objects.get(name=author_name)
+objects.filter(author=author)
