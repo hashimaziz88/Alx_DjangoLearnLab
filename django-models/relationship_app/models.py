da13d7a9ab818f5baaeaefd61fd354916b7b79e8
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-08-16 15:08:04 +0200

    commits

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -9,11 +9,20 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return self.name
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
