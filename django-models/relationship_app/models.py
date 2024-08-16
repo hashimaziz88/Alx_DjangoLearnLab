8f13226c8ca69d6bc110a00ae31313024eb107c5
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-08-16 15:52:26 +0200

    commits task 1

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -12,12 +15,14 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
+
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
 
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
 # Create your views here.
