83489687cef223b6b1e20b0b1797e48ed20ce4c0
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-08-27 13:20:49 +0200

    commits changes

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+
 ]
