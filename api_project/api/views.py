4ecc11f4d4aaf8ead0bea4520cc76fd3637ad10c
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-08-27 12:37:17 +0200

    changes task 1 and task 2 commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 # Create your views here.
