d946198cd2a5d26dfb8fda0c8c9e053c9376e4e6
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-08 17:38:39 +0200

    update urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List and create books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve, update, delete by ID
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
 ]
