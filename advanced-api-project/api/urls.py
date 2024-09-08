0d3114fa338eb9cf309626e93e3756b286f45ecb
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-08 17:38:01 +0200

    changes urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ from .views import BookCreateView, BookUpdateView, BookDeleteView
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List and create books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve, update, delete by ID
-    path('books/new/', BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book-update'),  # Update a book
+    path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
 ]
