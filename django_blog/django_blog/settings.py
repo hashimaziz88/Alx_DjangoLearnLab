bb1c0faf07700c2b26a647683307713a2e76ca47
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-09 16:37:10 +0200

    update settings

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -81,6 +81,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "USER": "hashim",
+        "HOST": "localhost",
     }
 }
 
