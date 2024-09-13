f54b5f8769b7a6704d1c16f6986269a8b96f6a92
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-13 19:36:05 +0200

    commits 3

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/comment/', views.add_comment, name='add_comment')
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete')
 ]
