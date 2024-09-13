f6cbc381b206f0736fa6ad04d766b0f5776ad2d1
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-13 19:24:43 +0200

    chganges 2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -26,15 +26,36 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(UpdateView):
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+
+# Post Update View
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
-    template_name = 'blog/post_form.html'
+    template_name = 'blog/post_form.html'  # Adjust to your template
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class PostDeleteView(DeleteView):
+    # Ensure only the author can edit the post
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Post Delete View
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post_list')
+    success_url = '/'
+    template_name = 'blog/post_confirm_delete.html'  # Adjust to your template
+
+    # Ensure only the author can delete the post
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
 
 @login_required
 def profile(request):
