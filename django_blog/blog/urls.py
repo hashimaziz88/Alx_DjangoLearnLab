ba1fc619df1ffc2c67f57217893eba5e80e82d96
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-13 19:22:49 +0200

    changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
