cf528a0553701a83266508614450a274727d72cb
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-13 19:19:13 +0200

    commits

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']  # Add any other fields you want to allow updates for
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import ProfileUpdateForm
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, 'blog/profile.html', {'form': form})
+
 
 def register(request):
     if request.method == 'POST':
