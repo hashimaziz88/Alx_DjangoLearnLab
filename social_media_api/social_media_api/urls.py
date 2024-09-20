4d5c81fa825e84d209890e875dacb55444e58e8f
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-20 21:22:25 +0200

    changes task 1 urls

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/posts', include('social_django.urls', namespace='social')),
+    path('api/posts', include('posts.urls', namespace='posts')),
 ]
