f1f8af2e803647cc93a52cec9836d5b1339fed05
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-20 20:53:52 +0200

    commits changes task 0

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -8,3 +8,6 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
     followers = models.ManyToManyField("self", symmetrical=False, related_name="following")
+
+    def __str__(self):
+        return self.username
--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,38 +1,48 @@
+# accounts/serializers.py
+
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
 
 User = get_user_model()
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()  # Custom user model
-        fields = ['username', 'email', 'bio', 'profile_picture']
-        read_only_fields = ['username', 'email']  # Make these read-only, assuming you don't want them updated
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
+            profile_picture=validated_data.get('profile_picture', None),
         )
+        Token.objects.create(user=user)  # Create a token for the new user
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username = data.get("username")
+        password = data.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials")
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return {'token': token.key}
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'bio', 'profile_picture']
